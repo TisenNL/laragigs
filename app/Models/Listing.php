e6c4d014e390b09198f649bedf958ccf4eead3d6
Tiago Neves Leao Costa Lino <tiagobrasil18@gmail.com>
2023-01-17 14:10:00 -0300

    study session 17-01

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,33 +1,11 @@
 <?php
+
 namespace App\Models;
 
-class Listing {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis maiores pariatur recusandae 
-                cupiditate mollitia esse voluptatibus dolorum laborum harum dicta optio, molestiae corporis error incidunt. 
-                Nobis repellendus quos voluptatem asperiores!'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis maiores pariatur recusandae 
-                cupiditate mollitia esse voluptatibus dolorum laborum harum dicta optio, molestiae corporis error incidunt. 
-                Nobis repellendus quos voluptatem asperiores!'
-            ]
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id) {
-        $listings = self::all();
-
-        foreach($listings as $listing) {
-            if($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
